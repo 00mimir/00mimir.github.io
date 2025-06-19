0523cb67a25fdbb1254a797772c27fe342ebf71a
00mimir <00mimir@tuta.io>
2025-06-19 15:39:15 -0300

    a lot of tests

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const blogList = document.getElementById("blog-post-list")
+const text = document.createElement("p")
+const textContent = document.createTextNode("lksdjfhaslkdjfhalskjdfhlaskjdfhlkasjdhflk")
+text.appendChild(textContent)
+
+blogList.append(text)
