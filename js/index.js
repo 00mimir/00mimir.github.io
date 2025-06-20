091b18bf67fef233a6acfe4296d173a36c0f9308
00mimir <00mimir@tuta.io>
2025-06-20 10:04:42 -0300

    few changes

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const blogList = document.getElementById("blog-post-list")
-const text = document.createElement("p")
-const textContent = document.createTextNode("lksdjfhaslkdjfhalskjdfhlaskjdfhlkasjdhflk")
-text.appendChild(textContent)
-
-blogList.append(text)
